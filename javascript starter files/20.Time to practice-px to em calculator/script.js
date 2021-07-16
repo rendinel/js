7281d9f1e67b010d40fd6042d75b11d3dc44156c
rendinel <rendinel@protonmail.com>
2021-07-16 18:23:45 +0200

    push

--- javascript starter files/20.Time to practice-px to em calculator/script.js
+++ javascript starter files/20.Time to practice-px to em calculator/script.js
@@ -1,1 +1,16 @@
 // em = required / parent 
+
+var calculate = () => {
+    var parentVal = document.getElementById('parent').value;
+    var requireVal = document.getElementById('required').value;
+    var result = document.getElementById('result');
+    // check to see if input are empty
+    if(!parentVal | !requireVal){
+        alert('Please fill in all fields')
+    } else {
+        result.innerHTML = requireVal / parentVal;
+    }
+}
+
+var btn = document.querySelector('button');
+btn.addEventListener('click', calculate);
