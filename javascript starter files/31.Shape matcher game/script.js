6301942c6a021fc3746977e77f42b2438c4e7f8c
rendinel <rendinel@protonmail.com>
2021-07-18 18:51:26 +0200

    push

--- javascript starter files/31.Shape matcher game/script.js
+++ javascript starter files/31.Shape matcher game/script.js
@@ -11,16 +11,20 @@
 
 // console.log(currentScore + ' outside');
 
+//we set the start score at 0,the playing status to false,we initialize the 2 shape without assign them and we incapsulate the matchbtn inside a const
 let currentScore = 0;
 let playing = false;
 let shape1;
 let shape2;
+const matchBtn = document.getElementById('match');
 
 //const means constant and the cannot be updated if we try to update it will cause an error
 // const number = 10;
 // number++;
 // console.log(number);
 
+
+//this is the object of the shape we are goinmg to match
 const shapes = [
     {color:'#FF595E',width:250, height:160},
     {color:'#FF595E',width:270, height:160},
@@ -34,4 +38,66 @@ const shapes = [
     {color:'#6A4C93',width:430, height:180}
 ];
 
-console.log(shapes[3]);
+//function that generate a random shapes from the shapes object,we generate a random number and assign it to the index
+const selectRandomShape = () => {
+    const randomNum = Math.floor(Math.random()*shapes.length);
+    const randomSelection = shapes[randomNum];
+    return randomSelection;
+}
+
+// x = selectRandomShape();
+// console.log(x);
+
+//template literal
+// var name = 'chris';
+// console.log(`Hello ${name}`);
+// allow us to create multiline string with `` instead of ''
+// var name2 = `chrisaslidasoidhasoidhaosihdoaishd
+// aoidhjaosijdoasijd`;
+// console.log(`Hello ${name2}`);
+
+//this function with setInterval activate every second , it permit us to play the matchbtn everytime we start a game
+//it assign the 2 random shape generate above to the 2 let shape1 and shape2
+//with a template literal and .cssText pass css from the object corrisponding to the random shape selected to the 2 div insdide html
+
+const repeatRandomShape = () => {
+    setInterval(() => {
+        matchBtn.disabled = false;
+        shape1 = selectRandomShape();
+        shape2 = selectRandomShape();
+
+        const shape1Styles = `width:${shape1.width+'px'};
+                              background:${shape1.color};
+                              height:${shape1.height+'px'}`;
+        //with cssText we can pass some css to an alement from js
+        document.getElementById('shape1').style.cssText = shape1Styles;
+        const shape2Styles = `width:${shape2.width+'px'};
+        background:${shape2.color};
+        height:${shape2.height+'px'}`;
+
+        document.getElementById('shape2').style.cssText = shape2Styles;
+    }, 1000);
+}
+
+//start game,set the playing to true,disabled the btn play every time we start  a game and launch the repeat random shape function
+document.getElementById('play').onclick = () => {
+    playing = true;
+    document.getElementById('play').disabled = true;
+    repeatRandomShape();
+}
+
+//disable the matchbtn so we can press only one time for every shape, with Object.is we can compare 2 different property from the same object and return tue if they match
+// if the 2 object are = we add 1 to the score and print inside the right element otherwise we subtract and print it 
+document.getElementById('match').onclick = () => {
+    matchBtn.disabled = true;
+    if(playing){
+        if(Object.is(shape1, shape2)){
+            currentScore++;
+            document.getElementById('score').innerHTML = currentScore;
+        } else {
+            currentScore--;
+            document.getElementById('score').innerHTML = currentScore;
+        }
+    }
+}
+
